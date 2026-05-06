f6efd88d843cba3e134de7cf9581b9e2f50156a3
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-05-06 15:36:57 +0530

    Refactor Terraform configuration for dev environment
    Updated resource group names and added local variables for better organization.

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,7 +1,3 @@
-########################################
-# Terraform + Backend + Provider
-########################################
-
 terraform {
   required_providers {
     azurerm = {
@@ -14,7 +10,7 @@ terraform {
     resource_group_name  = "rg-dalberg-terraform-state"
     storage_account_name = "tfstatedalbergdevstr"
     container_name       = "tfstate"
-    key                  = "avd-dev.tfstate"
+    key                  = "dalberg-dev-avd.tfstate"
   }
 }
 
@@ -22,63 +18,51 @@ provider "azurerm" {
   features {}
 }
 
-########################################
-# Create Resource Groups 
-########################################
+locals {
+  client_name   = "dalberg"
+  environment   = "dev"
+  location      = "Central India"
+  location_code = "cin"
+
+  prefix = "${local.client_name}-${local.environment}-${local.location_code}"
+
+  tags = {
+    client      = local.client_name
+    environment = local.environment
+    managed_by  = "terraform"
+  }
+}
 
 resource "azurerm_resource_group" "network" {
-  name     = "rg-network-dev"
-  location = "Central India"
+  name     = "${local.prefix}-rg-network"
+  location = local.location
+
+  tags = local.tags
 }
 
 resource "azurerm_resource_group" "avd" {
-  name     = "rg-avd-dev"
-  location = "Central India"
-}
+  name     = "${local.prefix}-rg-avd"
+  location = local.location
 
-########################################
-# Network Module
-########################################
+  tags = local.tags
+}
 
 module "network" {
   source = "git::https://github.com/darshanthenge03-cloud/terraform-azure-modules.git//network"
 
   resource_group_name = azurerm_resource_group.network.name
-  location            = azurerm_resource_group.network.location
+  location            = local.location
 
+  vnet_name = "${local.prefix}-vnet"
   vnet_cidr = "10.0.0.0/16"
 
-  public_subnets = {
-    "public-1" = "10.0.1.0/24"
+  subnets = {
+    "${local.prefix}-snet-avd"      = "10.0.1.0/24"
+    "${local.prefix}-snet-ad"       = "10.0.2.0/24"
+    "${local.prefix}-snet-bastion"  = "10.0.3.0/27"
+    "GatewaySubnet"                 = "10.0.4.0/27"
   }
 
-  private_subnets = {
-    "app-subnet" = "10.0.2.0/24"
-  }
-
-  bastion_subnet_cidr = "10.0.3.0/27"
-  gateway_subnet_cidr = "10.0.4.0/27"
+  tags = local.tags
 }
-
-########################################
-# AVD Module
-########################################
-
-module "avd" {
-  source = "git::https://github.com/darshanthenge03-cloud/terraform-azure-modules.git//avd"
-
-  subnet_id = module.network.private_subnet_ids["app-subnet"]
-
-  host_pool_name      = "avd-hp-dev"
-  app_group_name      = "avd-dag-dev"
-  workspace_name      = "avd-ws-dev"
-
-  resource_group_name = azurerm_resource_group.avd.name
-  location            = azurerm_resource_group.avd.location
-
-  session_host_count = 1
-  vm_size            = "Standard_D4s_v5"
-
-  admin_username = var.admin_username
-  admin_password = var.admin_password
 }
